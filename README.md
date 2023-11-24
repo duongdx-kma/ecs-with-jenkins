85e7755202585c7b6fad923f6fc32bf0833e04a9
duongdx <dinh.xuan.duong@sun-asterisk.com>
2023-11-24 09:35:13 +0700

    push docker file

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 curl --proto "https" -o "/tmp/ecs-anywhere-install.sh" "https://amazon-ecs-agent.s3.amazonaws.com/ecs-anywhere-install-latest.sh" && bash /tmp/ecs-anywhere-install.sh --region "ap-southeast-1" --cluster "express-app-ecs" --activation-id "37d63c3b-47aa-4071-8989-41277ed1704e" --activation-code "CwBzS8bAtXDb6BRMJNEJ"
+
+1. create network
+> docker create network mongodb-network
+2. create express app:
+> docker build -t ecs_app  .
+> docker run --name ecs_app -p 8089:8088 -d --network mongodb-network ecs_app
+3. start mongo
+> docker run --name mongodb \
+-e MONGO_INITDB_ROOT_USERNAME=root \
+-e MONGO_INITDB_ROOT_PASSWORD=password \
+-e MONGO_INITDB_DATABASE=my-db \
+--network mongodb-network \
+-p 27017:27017 \
+-d mongo
+
+3. start mongo-express: https://stackoverflow.com/questions/60748269/mongo-express-started-by-docker-compose-causing-error-mongoerror-failed-to-co
+> docker run --name mongodb-express -p 8086:8081 \
+-e ME_CONFIG_MONGODB_ADMINUSERNAME=root \
+-e ME_CONFIG_MONGODB_ADMINPASSWORD=password \
+-e ME_CONFIG_MONGODB_URL=mongodb://root:password@mongodb:27017/ \
+-e ME_CONFIG_BASICAUTH_USERNAME=root \
+-e ME_CONFIG_BASICAUTH_PASSWORD=password \
+--network mongodb-network \
+-d mongo-express
--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import express from 'express'
 import os from 'os'
+import bodyParser from 'body-parser';
+import {MongoClient} from 'mongodb'
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const app = express()
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
 
 app.get('/', (request, response) => {
     const clientIp = request.header('x-forwarded-for');
@@ -20,6 +30,66 @@ app.get('/', (request, response) => {
     })
 })
 
-app.listen(8088, () => {
+// use when starting application locally
+let mongoUrlLocal = process.env.MONGODB_URL;
+
+// pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
+let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+
+// "user-account" in demo with docker. "my-db" in demo with docker-compose
+let databaseName = process.env.DATABASE_NAME;
+
+app.post('/update-profile', function (req, res) {
+    let userObj = req.body;
+
+    MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+        if (err) {
+            console.log('Connect error')
+            throw err;
+        }
+
+        let db = client.db(databaseName);
+        userObj['userid'] = 1;
+
+        let myquery = { userid: 1 };
+        let newvalues = { $set: userObj };
+
+        db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
+            if (err) throw err;
+            client.close();
+        });
+
+    });
+    // Send response
+    res.send(userObj);
+});
+
+app.get('/get-profile', function (req, res) {
+    console.log(11111, mongoUrlLocal);
+    console.log(mongoUrlLocal)
+    let response = {};
+    // Connect to the db
+    MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+        if (err) {
+            console.log('Connect error')
+            throw err;
+        }
+
+        let db = client.db(databaseName);
+
+        let myquery = { userid: 1 };
+
+        db.collection("users").findOne(myquery, function (err, result) {
+            if (err) throw err;
+            response = result;
+            client.close();
+
+            // Send response
+            res.send(response ? response : {});
+        });
+    });
+});
+
+app.listen(process.env.EXPRESS_PORT, () => {
     console.log('app started successfully')
 })
